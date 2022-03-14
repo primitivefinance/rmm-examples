79ea259414606fb0ae47a49612cd4cfc979db1f7
clemlak <clementlakhal@gmail.com>
2022-03-14 19:06:05 +0400

    test: add TestERC20 contract

---
+++ contracts/test/TestERC20.sol
@@ -0,0 +1,13 @@
+// SPDX-License-Identifier: GPL-3.0-only
+pragma solidity 0.8.6;
+
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+
+contract TestERC20 is ERC20("TestERC20", "TEST") {
+    function mint(
+        address account,
+        uint256 amount
+    ) external {
+        _mint(account, amount);
+    }
+}
