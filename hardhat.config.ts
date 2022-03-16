23584d6e54cc98c95116ec7404b2b6be98170f05
clemlak <clementlakhal@gmail.com>
2022-03-16 21:18:11 +0400

    feat: update block gas limit

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -6,7 +6,7 @@ import { HardhatUserConfig } from 'hardhat/config'
 const config: HardhatUserConfig = {
   networks: {
     hardhat: {
-      allowUnlimitedContractSize: true,
+      blockGasLimit: 30000000,
     },
     ganache: {
       url: 'http://127.0.0.1:8545',
