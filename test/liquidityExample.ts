17a2e9f7642971ce871918accb5a47407316b514
clemlak <clementlakhal@gmail.com>
2022-03-22 22:44:28 +0400

    test: update LiquidityExample test

--- test/liquidityExample.ts
+++ test/liquidityExample.ts
@@ -7,6 +7,7 @@ import expect from './shared/expect';
 import { DEFAULT_CALIBRATION } from './shared/config';
 
 let liquidityExample: Contract;
+let poolId: string;
 
 runTest('LiquidityExample', function () {
   beforeEach(async function () {
@@ -17,11 +18,12 @@ runTest('LiquidityExample', function () {
       this.contracts.risky.address,
       this.contracts.stable.address,
     );
+
+    poolId = DEFAULT_CALIBRATION.poolId(this.contracts.primitiveEngine.address);
   });
 
   describe('success cases', function () {
     it('allocates risky and stable into a pool', async function () {
-      /*
       await this.contracts.risky.approve(
         liquidityExample.address,
         constants.MaxUint256,
@@ -31,15 +33,22 @@ runTest('LiquidityExample', function () {
         liquidityExample.address,
         constants.MaxUint256,
       );
-*/
-      /*
+
+      const delLiquidity = parseWei(1).raw;
+      const res = await this.contracts.primitiveEngine.reserves(poolId)
+      const delRisky = delLiquidity.mul(res.reserveRisky).div(res.liquidity)
+      const delStable = delLiquidity.mul(res.reserveStable).div(res.liquidity)
+
       await liquidityExample.allocate(
-        DEFAULT_CALIBRATION.poolId,
-        parseWei(100).raw,
-        parseWei(100).raw,
-        0,
+        poolId,
+        delRisky,
+        delStable,
+        delLiquidity,
       );
-      */
+
+      expect(
+        await liquidityExample.liquidityOf(this.wallets.deployer.address)
+      ).to.be.equal(delLiquidity);
     });
   })
 });
