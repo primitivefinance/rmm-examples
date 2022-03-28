813225783f1337e2a4dede9dfc18cc609099dce4
clemlak <clementlakhal@gmail.com>
2022-03-28 12:53:12 +0400

    test: add Mocha custom hooks

--- types/index.d.ts
+++ types/index.d.ts
@@ -1,4 +1,4 @@
-import { Contract } from 'ethers';
+import { Contract, BigNumber } from 'ethers';
 import { SignerWithAddress } from '@nomiclabs/hardhat-ethers/signers';
 
 export interface Contracts {
@@ -20,5 +20,48 @@ declare module 'mocha' {
   export interface Context {
     contracts: Contracts;
     wallets: Wallets;
+  }
+}
+
+declare global {
+  export namespace Chai {
+    interface Assertion {
+      revertWithCustomError(errorName: string, params?: any[]): AsyncAssertion
+      updateMargin(
+        manager: Contract,
+        account: string,
+        engine: string,
+        delRisky: BigNumber,
+        riskyIncrease: boolean,
+        delStable: BigNumber,
+        stableIncrease: boolean
+      ): AsyncAssertion
+      increaseMargin(
+        manager: Contract,
+        account: string,
+        engine: string,
+        delRisky: BigNumber,
+        delStable: BigNumber
+      ): AsyncAssertion
+      decreaseMargin(
+        manager: Contract,
+        account: string,
+        engine: string,
+        delRisky: BigNumber,
+        delStable: BigNumber
+      ): AsyncAssertion
+      increasePositionLiquidity(
+        manager: Contract,
+        account: string,
+        poolId: string,
+        liquidity: BigNumber
+      ): AsyncAssertion
+      decreasePositionLiquidity(
+        manager: Contract,
+        account: string,
+        poolId: string,
+        liquidity: BigNumber
+      ): AsyncAssertion
+    }
   }
 }
