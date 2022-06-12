37cb2a3a86bf1d82d881f39496a769a89504e3f7
clemlak <clementlakhal@gmail.com>
2022-06-12 18:14:56 +0400

    test: update StakingModule tests

--- test/stakingModule.ts
+++ test/stakingModule.ts
@@ -4,6 +4,8 @@ import expect from './shared/expect';
 
 let alice, bob;
 let stakingModule;
+let rewardToken;
+let stakingToken;
 
 export async function mineBlocks(amount: number): Promise<void> {
   for (let i = 0; i < amount; ++i) {
@@ -11,49 +13,64 @@ export async function mineBlocks(amount: number): Promise<void> {
   }
 }
 
-
 describe('StakingModule', () => {
   beforeEach(async () => {
     [alice, bob] = hre.waffle.provider.getWallets();
 
+    const RewardToken = await hre.ethers.getContractFactory('TestERC20');
+    rewardToken = await RewardToken.deploy();
+
+    const StakingToken = await hre.ethers.getContractFactory('TestERC1155Permit');
+    stakingToken = await StakingToken.deploy();
+    await stakingToken.mint(alice.address, 0, 10);
+
     const StakingModule = await hre.ethers.getContractFactory('StakingModule');
-    stakingModule = await StakingModule.deploy();
-    await stakingModule.create(0, stakingModule.address, 100);
+    stakingModule = await StakingModule.deploy(rewardToken.address);
+    await stakingModule.create(0, stakingToken.address, 100);
   });
 
-  it('deposits into a pool', async () => {
-    await stakingModule.deposit(0, 100);
-    const user = await stakingModule.users(alice.address, 0);
-    expect(user.balance).to.be.equal(100);
-  });
+  describe('success cases', () => {
+    beforeEach(async () => {
+      await stakingToken.setApprovalForAll(stakingModule.address, true);
+      await stakingToken.connect(bob).setApprovalForAll(stakingModule.address, true);
+    });
 
-  it('deposits', async () => {
-    await stakingModule.deposit(0, 100);
-    await mineBlocks(2);
-    const pending = await stakingModule.pending(alice.address, 0);
-    expect(pending).to.be.equal(200);
-  });
+    it('deposits into a pool', async () => {
+      await stakingModule.deposit(0, 10);
+      const user = await stakingModule.users(alice.address, 0);
+      expect(user.balance).to.be.equal(10);
+    });
 
-  it('deposits', async () => {
-    await stakingModule.deposit(0, 100);
-    await stakingModule.connect(bob).deposit(0, 100);
+    it('deposits and checks the pending reward', async () => {
+      await stakingModule.deposit(0, 10);
+      await mineBlocks(2);
+      const pending = await stakingModule.pending(alice.address, 0);
+      expect(pending).to.be.equal(200);
+    });
 
-    {
-      const pendingOfAlice = await stakingModule.pending(alice.address, 0);
-      expect(pendingOfAlice).to.be.equal(100);
+    it('deposits', async () => {
+      await stakingToken.mint(bob.address, 0, 10);
 
-      const pendingOfBob = await stakingModule.pending(bob.address, 0);
-      expect(pendingOfBob).to.be.equal(0);
-    }
+      await stakingModule.deposit(0, 10);
+      await stakingModule.connect(bob).deposit(0, 10);
 
-    await mineBlocks(2);
+      {
+        const pendingOfAlice = await stakingModule.pending(alice.address, 0);
+        expect(pendingOfAlice).to.be.equal(100);
 
-    {
-      const pendingOfAlice = await stakingModule.pending(alice.address, 0);
-      expect(pendingOfAlice).to.be.equal(200);
+        const pendingOfBob = await stakingModule.pending(bob.address, 0);
+        expect(pendingOfBob).to.be.equal(0);
+      }
 
-      const pendingOfBob = await stakingModule.pending(bob.address, 0);
-      expect(pendingOfBob).to.be.equal(100);
-    }
+      await mineBlocks(2);
+
+      {
+        const pendingOfAlice = await stakingModule.pending(alice.address, 0);
+        expect(pendingOfAlice).to.be.equal(200);
+
+        const pendingOfBob = await stakingModule.pending(bob.address, 0);
+        expect(pendingOfBob).to.be.equal(100);
+      }
+    });
   });
 });
